c0411833d65bce80ed1edaae1da53eae3be424e7
Madison Evans <m.corbinevans@gmail.com>
2023-01-09 11:19:29 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+var upperCaseCustomerName = () => {
+  customerName = customerName.toUpperCase();
+};
+
+var bestCustomer;
+var setBestCustomer = () => {
+  bestCustomer = "not bob";
+};
+
+var overwriteBestCustomer = () => {
+  bestCustomer = "maybe bob";
+};
+const leastFavoriteCustomer = "not me :)";
+
+var changeLeastFavoriteCustomer = () => {
+  leastFavoriteCustomer = "me :(";
+};
